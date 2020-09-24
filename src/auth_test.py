a4e3d38a74677f03094fbae414589abac83f1542
gilbertzhuo <gilbertzhuo@gmail.com>
2020-09-24 22:34:50 +1000

    Created assumptions file

--- src/auth_test.py
+++ src/auth_test.py
@@ -1,13 +1,27 @@
 import pytest
 #Test Login
+
+#Check for valid Email
+#Email entered does not belong to a user
+#Password is not correct
 def test_login():
     assert auth_login(email, password) 
 
-#Test Logout
-def test_logout():
-    assert auth_logout(token)
 
+#Test Logout
 
 #Test Register
-def test_register():
-    assert auth_register(email, password, name_first, name_last)
+def test_email():
+    auth_register("","password", "Gilbert", "Zhuo") {
+        assert ()
+    }
+
+#Check for valid Email
+
+#Check if email has already been used by another user
+
+#Pasword entered is less than 6 characters
+
+#name_first is not between 1 and 50 characthers
+
+#name_last is not between 1 and 50 characters in length
