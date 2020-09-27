81e2346dd73b7357a1ba72aed433ecbcc4007db2
terry.h12 <terencehuang12@outlook.com>
2020-09-27 19:51:59 +1000

    Completed initial channels_tests

--- src/channels_test.py
+++ src/channels_test.py
@@ -4,18 +4,13 @@ from channels import *
 from error import InputError
 import pytest
 
-'''
-Needs work
-    - Check if the user is a member of other channels 
-'''
-
 def test_channels_list_empty():
     # Creating a valid account
     authorised_user = auth_register("validEmail@gmail.com", "valid_password", "valid_first", "valid_last")
     auth_login("valid_email@gmail.com", "valid_password")
     # Return empty, since no other channels
     channels_list = channels_list(authorised_user['token'])
-    assert channels_list['channels'] == {}
+    assert channels_list['channels'] == []
 
 def test_channels_list_simple():
     # Creating valid account and channel for authorised_user
@@ -29,10 +24,8 @@ def test_channels_list_simple():
     channel_invite(authorised_user['token'], channel_2['channel_id'], authorised_user['u_id'])
     # Obtain the list of channels of authorised_user
     channels_list = channels_list(authorised_user['token'])
-    
-    # !!! IDK what to do
 
-    assert channels_list['channels'] == {}
+    assert channels_list['channels'] == ['new_channel', 'new_channel2']
 
 def test_channels_listall_empty():
     # Creating a valid account
@@ -40,7 +33,7 @@ def test_channels_listall_empty():
     auth_login("valid_email@gmail.com", "valid_password")
     # Return empty, since no other channels
     channels_list = channels_list(authorised_user['token'])
-    assert channels_list['channels'] == {}
+    assert channels_list['channels'] == []
 
 def test_channels_listall_simple():
     # Creating valid account and channel for authorised_user
@@ -54,17 +47,17 @@ def test_channels_listall_simple():
     channel_invite(authorised_user['token'], channel_2['channel_id'], authorised_user['u_id'])
     # Obtain the list of all channels
     channels_list_all = channels_listall(authorised_user['token'])
-    # !!! If we create a global variable for channels 
-    whole_list = global_channels_list['channels']
-    # The gloabl list should be the same as channels_list
-    error = False
-    for dictionary in whole_list:
-        # List are incorrect
-        if channels_list_all['channels'] != dictionary:
-            error = True
-            break
 
-    assert error = False
+    assert channels_list['channels'] == ['new_channel', 'new_channel2']
+    # whole_list = global_channels_list['channels']
+    # # The gloabl list should be the same as channels_list
+    # error = False
+    # for dictionary in whole_list:
+    #     # List are incorrect
+    #     if channels_list_all['channels'] != dictionary:
+    #         error = True
+    #         break
+    # assert error = False
 
 def test_channels_create_fails():
     # Creating a valid account
