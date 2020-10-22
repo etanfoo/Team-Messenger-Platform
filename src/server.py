2047449334029e885af5c54d6a8a7d634768efee
Etan Foo <e.foo@student.unsw.edu.au>
2020-10-23 01:02:53 +1100

    wrote http user routes

--- src/server.py
+++ src/server.py
@@ -1,9 +1,10 @@
 import sys
 from json import dumps
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from error import InputError
 from channels import channels_list, channels_listall, channels_create
+from user import user_profile, user_profile_setname, user_profile_setemail, user_profile_sethandle
 
 def defaultHandler(err):
     response = err.get_response()
@@ -54,5 +55,42 @@ def http_channels_create():
     new_data = request.get_json()
     return dumps(channels_create(new_data["token"], new_data["name"], new_data["is_public"]))
 
+
+
+
+
+####################
+# User functions
+####################
+@APP.route('/user/profile', methods = ['GET'])
+def http_user_profile():
+    data = request.args
+    return jsonify(user_profile(data['token'], int(data['u_id'])))
+
+@APP.route('/user/profile/setname', methods = ['PUT'])
+def http_user_profile_setname():
+    data = request.get_json()
+    return jsonify(user_profile_setname(data['token'], data['name_first'], data['name_last']))
+
+
+@APP.route('/user/profile/setemail', methods = ['PUT'])
+def http_user_profile_setemail():
+    data = request.get_json()
+    return jsonify(user_profile_setemail(data['token'], data['email']))
+
+@APP.route('/user/profile/sethandle', methods = ['PUT'])
+def http_user_profile_sethandle():
+    data = request.get_json()
+    return jsonify(user_profile_sethandle(data['token'], data['handle_str']))
+
+
+
+
+
+
+
 if __name__ == "__main__":
     APP.run(port=0) # Do not edit this port
+
+
+
