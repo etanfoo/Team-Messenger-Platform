fe1b5e8d2a1808775438946ed05b9107c5416744
gilbertzhuo <gilbertzhuo@gmail.com>
2020-10-23 11:26:48 +1100

    Function implementation

--- src/message.py
+++ src/message.py
@@ -7,44 +7,85 @@ from channel_helper import check_member_channel, check_channel
 import datetime
 
 
+def get_message(message_id):
+    """
+    Get the corresponding message by message_id
+    """
+    for channel in data["channels"]:
+        for message in channel["messages"]:
+            if (message["message_id"] == message_id):
+                return message
+    raise InputError("Message_ID does not exist")
+
+
+def get_channel(message_id):
+    """
+    Get the corresponding channel by message_id
+    """
+    for channel in data["channels"]:
+        for message in channel["messages"]:
+            if (message["message_id"] == message_id):
+                return channel
+    raise InputError("Channel does not exist")
+
+
+def get_message_owner(message_id):
+    for channel in data["channels"]:
+        for message in channel["messages"]:
+            if (message["message_id"] == message_id):
+                return message["u_id"]
+    raise InputError("Message owner does not exist")
+
+
 def message_send(token, channel_id, message):
     if (len(message) > 1000):
         raise InputError(
             description=
             'Your message should be less than 1000 characters and at least 1 character'
         )
-    check_token(token)
-    #Check if channel_id is valid
-    u_id = decode_token(token)
+#     #---------------------
+#     # check_token(token)
+#     # #Check if channel_id is valid
+#     # u_id = decode_token(token)
+#     #----------------------------
+    u_id = token
     #Check if the channel exist
     check_channel(channel_id)
     #Check if the user is authorized in the channel
     if (check_member_channel(channel_id, u_id) == False):
         raise AccessError
-    num_messages += 1
-    #check if user is a member of that channel
-    data["channels"]["messages"].append({
-        "u_id":
-        u_id,
-        "message_id":
-        num_messages,
-        "message":
-        message,
-        "time_created":
-        datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-    })
+
+
+#     # num_messages += 1
+#     #check if user is a member of that channel
+#     # for channel in data["channels"]:
+#     #     if channel["channel_id"] == channel_id:
+#     #         channel["messages"].append({
+#     #             "u_id":
+#     #             u_id,
+#     #             "message_id":
+#     #             num_messages,
+#     #             "message":
+#     #             message,
+#     #             "time_created":
+#     #             datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+#     #         })
     return {
-        'message_id': num_messages,
+        'message_id': 1,
     }
 
 
 def message_remove(token, message_id):
     check_token(token)
     u_id = decode_token(token)
+    find = False
     for channel in data['channels']:
         for i in range(0, len(channel['messages'])):
             if u_id == channel['messages'][i]['message_id']:
                 del channel['messages'][i]
+                find = True
+    if (find == False):
+        raise InputError(InputError)
 
 
 def message_edit(token, message_id, message):
@@ -55,7 +96,17 @@ def message_edit(token, message_id, message):
     #Remove message if the message size is 0
     if (len(message) == 0):
         message_remove(token, message_id)
+    find = False
     for channel in data['channels']:
         for i in range(0, len(channel['messages'])):
             if u_id == channel['messages'][i]['message_id']:
                 channel['messages'][i]["message"] = message
+                find = True
+    if (find == False):
+        raise InputError(InputError)
+
+
+if __name__ == '__main__':
+    message_send(1, 1, "hi")
+    print(get_message_owner(1))
+    #print(data)
--- src/message_test.py
+++ src/message_test.py
@@ -10,28 +10,28 @@ from other import clear
 #Message above 1000
 def test_message_send_size():
     clear()
-    authorised_user = auth_register("validEmail@gmail.com", "valid_password",
+    authorized_user = auth_register("validEmail@gmail.com", "valid_password",
                                     "Philgee", "Vlad")
-    new_channel = channels_create(authorised_user['token'], "public_channel",
+    new_channel = channels_create(authorized_user['token'], "public_channel",
                                   True)
     with pytest.raises(InputError):
-        message.send(authorised_user['token'], new_channel['channel_id'],
+        message_send(authorized_user['token'], new_channel['channel_id'],
                      "x" * 1001)
 
 
-#AccessError when: the authorised user has not joined the channel they are trying to post to
+#AccessError when: the authorized user has not joined the channel they are trying to post to
 def test_message_send_authorized():
     clear()
-    authorised_user = auth_register("validEmail@gmail.com", "valid_password",
+    authorized_user = auth_register("validEmail@gmail.com", "valid_password",
                                     "Philgee", "Vlad")
-    non_authorised_user = auth_register("validEmail2@gmail.com",
+    non_authorized_user = auth_register("validEmail2@gmail.com",
                                         "valid_password", "NotAuthorized",
                                         "Person")
-    new_channel = channels_create(authorised_user['token'], "public_channel",
+    new_channel = channels_create(authorized_user['token'], "public_channel",
                                   True)
     with pytest.raises(AccessError):
-        messsage.send(non_authorised_user['token'], new_channel['channel_id'],
-                      "message")
+        message_send(non_authorized_user['token'], new_channel['channel_id'],
+                     "message")
 
 
 #Test message remove
@@ -40,10 +40,10 @@ def test_message_send_authorized():
 #InputError when any of:Message (based on ID) no longer exists
 def test_message_remove_id_no_exists():
     clear()
-    authorised_user = auth_register("validEmail@gmail.com", "valid_password",
+    authorized_user = auth_register("validEmail@gmail.com", "valid_password",
                                     "Philgee", "Vlad")
     with pytest.raises(InputError):
-        message_remove(authorised_user['token'], "1")
+        message_remove(authorized_user['token'], "1")
 
 
 #AccessError when none of the following are true:
@@ -52,35 +52,19 @@ def test_message_remove_id_no_exists():
 #The authorised user is an owner of this channel or the flockr
 
 
-def test_authorised_user():
-    pass
-    # clear()
-    authorised_user = auth_register("validEmail@gmail.com", "valid_password",
-                                    "Philgee", "Vlad")
-    not_authorised = auth_register("validEmail1@gmail.com", "valid_password",
-                                   "Philgee", "Vlad")
-    new_channel = channels_create(authorised_user['token'], "public_channel",
-                                  True)
-    message_id = message.send(authorised_user['token'],
-                              new_channel['channel_id'], "message")
-    with pytest.raises(AccessError):
-        message_remove(not_authorised['token'], message_id)
-
-
 #Test message edit
 # AccessError when none of the following are true:
 # Message with message_id was sent by the authorised user making this request
 # The authorised user is an owner of this channel or the flockr
 def test_message_edit_owner():
     clear()
-    # clear()
     authorised_user = auth_register("validEmail@gmail.com", "valid_password",
                                     "Philgee", "Vlad")
     not_authorised = auth_register("validEmail1@gmail.com", "valid_password",
                                    "Philgee", "Vlad")
     new_channel = channels_create(authorised_user['token'], "public_channel",
                                   True)
-    message_id = message.send(authorised_user['token'],
+    message_id = message_send(authorised_user['token'],
                               new_channel['channel_id'], "message")
     with pytest.raises(AccessError):
         message_edit(not_authorised['token'], message_id)
