a73aa498df9d2215b0c21e1be2ba7b2a31717e2e
z5309776 <z5309776@cse.unsw.edu.au>
2020-11-05 14:48:59 +1100

    finished intital tests. potentially wont have full coverage

--- src/standup_test.py
+++ src/standup_test.py
@@ -7,7 +7,7 @@ from standup import standup_start, standup_active, standup_send
 from error import AccessError, InputError
 from other import clear
 from auth import auth_login, auth_register
-from channel import channel_invite
+from channel import channel_invite, channel_messages
 from channels import channels_create
 from message import message_send
 
@@ -28,7 +28,7 @@ def test_start_invalid_channel():
     with pytest.raises(InputError):
         standup_start(authorised_user['token'], -1, 10)
 
-def test_send_invalid_inactive():
+def test_start_invalid_inactive():
     '''
     inputerror if standup is already active when being called
     '''
@@ -45,6 +45,47 @@ def test_send_invalid_inactive():
 
     with pytest.raises(InputError):
         standup_start(authorised_user['token'], channel['channel_id'], 50)
+
+def test_start_expected():
+    '''
+    test if standup start returns an exepected output
+    '''
+    clear()
+
+    # creating user and channel
+    authorised_user = auth_register("validEmail@gmail.com", "valid_password", "Philgee", "Vlad")
+    auth_login("validEmail@gmail.com", "valid_password")
+
+    channel = channels_create(authorised_user['token'], "new_channel", True)
+
+    standup_result = standup_start(authorised_user['token'], channel['channel_id'], 50)
+
+    type_check = isinstance(standup_result, dict)
+    assert type_check == True
+
+    assert 'time_finish' in standup_result.keys()
+    key_check = isinstance(standup_result, int)
+    assert key_check == True
+
+def test_standup_empty():
+    '''
+    test if expected output occurs when no message is sent during standup
+    if no message is sent, standup will not be sent as a message
+    '''
+    clear()
+
+    # creating user and channel
+    authorised_user = auth_register("validEmail@gmail.com", "valid_password", "Philgee", "Vlad")
+    auth_login("validEmail@gmail.com", "valid_password")
+
+    channel = channels_create(authorised_user['token'], "new_channel", True)
+
+    standup_start(authorised_user['token'], channel['channel_id'], 1)
+
+    sleep(2)
+
+    check_messages = channel_messages(authorised_user['token'], channel['channel_id'], 0)
+    assert len(check_messages['messages']) == 0
 
 
 '''
@@ -63,6 +104,32 @@ def test_active_invalid_channel():
     # checking for inputerror
     with pytest.raises(InputError):
         standup_active(authorised_user['token'], -1)
+
+def test_active_expected():
+    '''
+    test if standup start returns an exepected output
+    '''
+    clear()
+
+    # creating user and channel
+    authorised_user = auth_register("validEmail@gmail.com", "valid_password", "Philgee", "Vlad")
+    auth_login("validEmail@gmail.com", "valid_password")
+
+    channel = channels_create(authorised_user['token'], "new_channel", True)
+
+    standup_result = standup_active(authorised_user['token'], channel['channel_id'])
+
+    type_check = isinstance(standup_result, dict)
+    assert type_check == True
+
+    assert 'is_active' in standup_result.keys()
+    assert 'time_finish' in standup_result.keys()
+
+    is_active_check = isinstance(standup_result['is_active'], bool)
+    time_finish_check = isinstance(standup_result['is_active'], int)
+
+    assert is_active_check == True
+    assert time_finish_check == True
 
 
 '''
@@ -131,3 +198,42 @@ def test_send_invalid_user():
 
     with pytest.raises(AccessError):
         standup_send(1, channel['channel_id'], "invalid user")
+
+def test_send_expected():
+    '''
+    test if standup start returns an exepected output
+    '''
+    clear()
+
+    # creating user and channel
+    authorised_user = auth_register("validEmail@gmail.com", "valid_password", "Philgee", "Vlad")
+    auth_login("validEmail@gmail.com", "valid_password")
+
+    channel = channels_create(authorised_user['token'], "new_channel", True)
+
+    standup_result = standup_send(authorised_user['token'], channel['channel_id'], "hello!!")
+
+    type_check = isinstance(standup_result, dict)
+    assert type_check == True
+
+def test_send_message_expected():
+    '''
+    test if standup send has an expected output when adding messages to a channel
+    '''
+    clear()
+
+    # creating user and channel
+    authorised_user = auth_register("validEmail@gmail.com", "valid_password", "Philgee", "Vlad")
+    auth_login("validEmail@gmail.com", "valid_password")
+
+    channel = channels_create(authorised_user['token'], "new_channel", True)
+
+    standup_start(authorised_user['token'], channel['channel_id'], 10)
+
+    standup_send(authorised_user['token'], channel['channel_id'], "testing standup")
+
+    sleep(1.5)
+
+    check_messages = channel_messages(authorised_user['token'], channel['channel_id'], 0)
+
+    assert len(check_messages['messages']) == 1
